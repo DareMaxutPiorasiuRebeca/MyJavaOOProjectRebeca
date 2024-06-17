69cd0261999d638dafa3f9312fd05cbde2a7056e
Rebeca <rebecutza7@yahoo.com>
2024-06-17 22:34:00 +0300

    f1comm

---
+++ src/ClassDemo.java
@@ -0,0 +1,7 @@
+public class ClassDemo {
+    private String atribute1;
+    public String atribute2;
+    public String getAtribute1 (){
+        return atribute1;
+    }
+}
--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,8 @@
 public class Human {
+    String email;
+    String adress;
+    public void verify(){
+    System.out.println("Verifies email");}
+    public void sendEmail(){
+    System.out.println("Sends email");}
 }
