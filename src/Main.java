e6a5011d894e833e078d6ccc319b6eea8e1b9200
Rebeca <rebecutza7@yahoo.com>
2024-06-17 22:35:04 +0300

    f1comm

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        ClassDemo obiect1 = new ClassDemo();
+        obiect1.atribute2="ceva";
+        obiect1.getAtribute1() = "altceva";
+
+        Human rebeca = new Human();
+        Human nicu = new Human();
+        rebeca.email = "rebeca@test.com";
+        nicu.email = "nicu@test.com";
+        System.out.println(rebeca.email);
+        System.out.println(nicu.email);
+    }
+}
